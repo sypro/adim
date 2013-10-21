918d8280a3998685f4d7c33f94c6bcdd988194fb
Ivan Pushkin(MetalGuardian) <imetalguardi@gmail.com>
2013-10-21 19:05:32 +0300

    add configuration model for testing

---
+++ backend/modules/configuration/components/ConfigurationForm.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: Ivan Pushkin
+ * Email: metal@vintage.com.ua
+ */
+
+namespace configuration\components;
+
+use core\components\FormModel;
+
+/**
+ * Class ConfigurationForm
+ * @package configuration\components
+ */
+class ConfigurationForm extends FormModel
+{
+}
