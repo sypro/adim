0e5d27959c63c8fc4f0c04fdaea5ebc22eda4119
Ivan Pushkin <imetalguardi@gmail.com>
2013-11-20 14:49:14 +0000

    add js to the application.js

--- frontend/www/js/application.js
+++ frontend/www/js/application.js
@@ -1,3 +1,106 @@
-function processRequest(response){if(response.replaces instanceof Array){for(var i=0,ilen=response.replaces.length;i<ilen;i++){$(response.replaces[i].what).replaceWith(response.replaces[i].data);}}if(response.js){$("body").append(response.js);}}
+function parseResponse(response)
+{
+	if (response.refresh) {
+		window.location.reload(true);
+	}
+	if (response.redirect) {
+		window.location.href = response.redirect;
+	}
+	if(response.replaces instanceof Array)
+	{
+		for(var i = 0, ilen = response.replaces.length; i < ilen; i++)
+		{
+			$(response.replaces[i].what).replaceWith(response.replaces[i].data);
+		}
+	}
+	if(response.append instanceof Array)
+	{
+		for(i = 0, ilen = response.append.length; i < ilen; i++)
+		{
+			$(response.append[i].what).append(response.append[i].data);
+		}
+	}
+	if(response.js)
+	{
+		$("body").append(response.js);
+	}
+}
 $(function(){
+	$(document).on('submit', '.ajax-form', function (event) {
+		event.preventDefault();
+		var that = this;
+		jQuery.ajax({'cache': false, 'type': 'POST', 'dataType': 'json', 'data':$(that).serialize(), 'success': function (response) {
+			parseResponse(response);
+		}, 'error': function (response) {
+			alert(response.responseText);
+		}, 'beforeSend': function() {
+
+		}, 'complete': function() {
+
+		}, 'url': this.action});
+		return false;
+	});
+	$(document).on('click', '.submit-form-link', function (event) {
+		var that = this;
+		if(!$(that).data('confirm') || confirm($(that).data('confirm'))) {
+			submitForm(
+				that,
+				that.href,
+				$(that).data('params')
+			);
+			return false;
+		} else {
+			return false;
+		}
+	});
+	$(document).on('click', '.ajax-link', function (event) {
+		event.preventDefault();
+		var that = this;
+		jQuery.ajax({'cache': false, 'type': 'POST', 'dataType': 'json', 'data':$(that).data('params'), 'success': function (response) {
+			parseResponse(response);
+		}, 'error': function (response) {
+			alert(response.responseText);
+		}, 'beforeSend': function() {
+
+		}, 'complete': function() {
+
+		}, 'url': that.href});
+		return false;
+	});
 });
+// yii submit form
+function submitForm (element, url, params) {
+	var f = $(element).parents('form')[0];
+	if (!f) {
+		f = document.createElement('form');
+		f.style.display = 'none';
+		element.parentNode.appendChild(f);
+		f.method = 'POST';
+	}
+	if (typeof url == 'string' && url != '') {
+		f.action = url;
+	}
+	if (element.target != null) {
+		f.target = element.target;
+	}
+
+	var inputs = [];
+	$.each(params, function(name, value) {
+		var input = document.createElement("input");
+		input.setAttribute("type", "hidden");
+		input.setAttribute("name", name);
+		input.setAttribute("value", value);
+		f.appendChild(input);
+		inputs.push(input);
+	});
+
+	// remember who triggers the form submission
+	// this is used by jquery.yiiactiveform.js
+	$(f).data('submitObject', $(element));
+
+	$(f).trigger('submit');
+
+	$.each(inputs, function() {
+		f.removeChild(this);
+	});
+}
