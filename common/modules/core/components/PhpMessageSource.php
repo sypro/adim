421177a6230505a188b6706674d6045c36a4fdea
MetalGuardian <imetalguardi@gmail.com>
2014-03-25 11:50:32 +0200

    change message source in backend

---
+++ common/modules/core/components/PhpMessageSource.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Author: Ivan Pushkin
+ * Email: metal@vintage.com.ua
+ */
+
+namespace core\components;
+
+use CPhpMessageSource;
+
+/**
+ * Class PhpMessageSource
+ * @package core\components
+ */
+class PhpMessageSource extends CPhpMessageSource
+{
+}
